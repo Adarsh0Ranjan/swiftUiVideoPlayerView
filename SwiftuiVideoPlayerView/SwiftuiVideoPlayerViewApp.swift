348586869a14b0d2fca6caf05ebc6b5cef0efe29
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2023-12-21 17:11:08 +0530

    Initial Commit

---
+++ SwiftuiVideoPlayerView/SwiftuiVideoPlayerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiVideoPlayerViewApp.swift
+//  SwiftuiVideoPlayerView
+//
+//  Created by Adarsh Ranjan on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiVideoPlayerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
